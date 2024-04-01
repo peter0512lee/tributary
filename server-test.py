e8cb844f3511f6e88575e4dfbd17e668a84786f7
peter0512lee <peter0512lee@gmail.com>
2024-04-01 20:19:12 +0800

    task 4

--- server-test.py
+++ server-test.py
@@ -1,8 +1,11 @@
 import requests as requests
 
 data = {
-    "engine_temperature": 0.3,
+    "engine_temperature": 0.12,
 }
 
 response = requests.post("http://0.0.0.0:8000/record", json=data)
+print(response.content)
+
+response = requests.post("http://0.0.0.0:8000/collect")
 print(response.content)
